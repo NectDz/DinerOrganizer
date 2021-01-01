6cc1efa6a103338379e5ace73d6f9667170a74e4
NectDz <54990613+NectDz@users.noreply.github.com>
2021-01-01 14:11:04 -0500

    Create intros.py

---
+++ intros.py
@@ -0,0 +1,17 @@
+import time
+
+def main_intro():
+    print("    Welcome to Kevin's Diner Organizer")
+    time.sleep(0.2)
+    print("      _____ _____ _   _ ______ _____  ")
+    print("     |  __ \_   _| \ | |  ____|  __ \  ")
+    time.sleep(0.1)
+    print("     | |  | || | |  \| | |__  | |__) | ")
+    print("     | |  | || | | . ` |  __| |  _  /  ")
+    time.sleep(0.1)
+    print("     | |__| || |_| |\  | |____| | \ \  ")
+    print("     |_____/_____|_| \_|______|_|  \_\ ")
+    time.sleep(0.1)
+    print("         Coded by Kevin Granados\n")
+    time.sleep(1)
+
